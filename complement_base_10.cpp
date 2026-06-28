02dd243f0baeb19abb32ee9a21e26f8a781b367c
Riddhi Mankar <riddhimankar851@gmail.com>
2026-06-28 19:13:24 +0530

    complement_base_10

---
+++ complement_base_10.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class Solution{
+public:
+int bitwiseComplement(int n){
+    if(n == 0){
+        return 1;
+    }
+    int mask = 0;
+    int temp =n;
+
+    while(temp != 0){
+        mask = (mask << 1) | 1;
+        temp >> 1;
+    }
+
+    return (~n) & mask;
+}
+};
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    Solution s;
+    s.bitwiseComplement(n);
+    return 0;
+}
